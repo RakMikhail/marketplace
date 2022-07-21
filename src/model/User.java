01797745712e6414daa381b7b9b495b96d520546
Mikhail Rak <misha.rak.2401@gmail.com>
2022-07-21 23:25:31 +0300

    add class User

---
+++ src/model/User.java
@@ -0,0 +1,35 @@
+package model;
+
+public class User {
+    private final int id;
+    private  String firstName;
+    private  String lastName;
+    private double amountOfMoney;
+
+    public User(int id, String firstName, String lastName, double amountOfMoney) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.amountOfMoney = amountOfMoney;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getAmountOfMoney() {
+        return amountOfMoney;
+    }
+
+    public void setAmountOfMoney(double amountOfMoney) {
+        this.amountOfMoney = amountOfMoney;
+    }
+}
