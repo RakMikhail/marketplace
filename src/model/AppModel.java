644065371e7817e8b16bf945cd536f4745193c05
Mikhail Rak <misha.rak.2401@gmail.com>
2022-07-22 18:19:41 +0300

    add functionality to show all users

--- src/model/AppModel.java
+++ src/model/AppModel.java
@@ -1,5 +1,10 @@
 package model;
 
+import utils.AppStarter;
+import views.UserView;
+
+import java.util.HashMap;
+
 public class AppModel {
     private int choice;
 
@@ -12,7 +17,9 @@ public class AppModel {
     }
 
     public void showUsers(){
-
+    UserView userView = new UserView();
+    userView.showAllUser(getUsers());
+    AppStarter.startApp();
     }
 
     public void showProducts(){
@@ -26,5 +33,16 @@ public class AppModel {
     }
     public void showListOfUsersByProduct(){
 
+    }
+
+    public static HashMap<Integer,User> getUsers(){
+        HashMap<Integer,User> users = new HashMap<>();
+        User user1 = new User(1,"Mikhailo","Rak",100.50);
+        User user2 = new User(2,"Vadym","Polishchuk",150.50);
+        User user3 = new User(3,"Andrii","Bodnar",80.50);
+        users.put(user1.getId(), user1);
+        users.put(user2.getId(), user2);
+        users.put(user3.getId(), user3);
+        return users;
     }
 }
---
+++ src/views/UserView.java
@@ -0,0 +1,19 @@
+package views;
+
+import model.User;
+
+import java.util.HashMap;
+
+import static utils.Rounder.roundValue;
+
+public class UserView {
+    public void showAllUser(HashMap<Integer, User> users){
+        users.forEach((id,value) -> System.out.printf("""
+    id : %d
+    name : %s %s
+    amount of money : %s
+    
+    
+    """,id,value.getFirstName(),value.getLastName(),roundValue(value.getAmountOfMoney())));
+    }
+}
